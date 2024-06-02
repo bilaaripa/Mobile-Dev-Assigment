bf89f8b513325ff1cd066e145bd4c1b270ee4f52
Salsabila Fatma Aripa <114417418+bilaaripa@users.noreply.github.com>
2024-06-03 00:48:26 +0700

    update be

--- cameraBE/run.py
+++ cameraBE/run.py
@@ -1,22 +1,10 @@
-import pyngrok.ngrok
+import ngrok
+
 from flaskr import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:5000"}})
 
 if __name__ == '__main__':
-    # Start the Flask app
-    app.run(debug=True, host='0.0.0.0', port=5000)
-    
-    # Start ngrok tunnel
-    url = pyngrok.ngrok.connect(5000)
-    print(f" * ngrok tunnel URL: {url}")
-
-    # Keep the program running
-    try:
-        while True:
-            pass
-    except KeyboardInterrupt:
-        pyngrok.ngrok.disconnect(url)
-        print(" * ngrok tunnel closed")
+    app.run(debug=True)
